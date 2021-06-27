c6f32e940c0f5f6342a218e516f3c7c3e307c4c0
chris <chrisclarkbell95@gmail.com>
2021-06-27 17:04:05 -0600

    add prettier.js formatter to project

--- README.md
+++ README.md
@@ -27,6 +27,14 @@ To learn more about Next.js, take a look at the following resources:
 
 You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
 
+## Code Styling
+
+This project use the [prettier.js](https://prettier.io/) code formatter. To code format the code accordingly, use the following command...
+
+```
+yarn prettier --write .
+```
+
 ## Deploy on Vercel
 
 The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -10,18 +10,12 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to Front Range
-        </h1>
+        <h1 className={styles.title}>Welcome to Front Range</h1>
 
-        <p className={styles.description}>
-          An engineering blog by Chris Bell
-        </p>
+        <p className={styles.description}>An engineering blog by Chris Bell</p>
       </main>
 
-      <footer className={styles.footer}>
-        Work in Progress
-      </footer>
+      <footer className={styles.footer}>Work in Progress</footer>
     </div>
-  )
+  );
 }
