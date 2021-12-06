6cdef4bf2df60200be61036f09f12ac4f51bd73e
chrisclarkbell <chrisclarkbell95@gmail.com>
2021-12-05 17:09:24 -0700

    fix

--- pages/index.js
+++ pages/index.js
@@ -3,33 +3,35 @@ import { getSortedPostsData } from '../lib/posts'
 
 const Profile = () => {
   return (
-    <div class="flex flex-col items-center justify-center">
-      <img class="rounded-full h-36 w-36" src="/profile.png"/>
-      <h1 class="prose prose-2xl font-black">Christopher Bell</h1>
-      <h1 class="prose prose-sm thin">Software Engineer</h1>
+    <div className="flex flex-col items-center justify-center">
+      <img className="rounded-full h-36 w-36" src="/profile.png"/>
+      <h1 className="prose prose-2xl font-black">Christopher Bell</h1>
+      <h1 className="prose prose-sm thin">Software Engineer</h1>
     </div>
   )
 }
 
 const Article = ({ id, title, date, description }) => {
   return (
-    <div class="m-2">
-      <h1 class="prose prose-sm thin">{date}</h1>
-      <h1 class="prose-xl font-black">{title}</h1>
+    <div className="m-2">
+      <h1 className="prose prose-sm thin">{date}</h1>
+      <h1 className="prose-xl font-black">{title}</h1>
       <h1>{description}</h1>
-      <h1 class="text-purple-600 font-semibold">Read full article</h1>
+      <Link href="/articles/{id}">
+        <h1 className="text-purple-600 font-semibold">Read full article</h1>
+      </Link>
     </div>
   )
 }
 
-export default ({ allPostsData }) => {
+const Home = ({ allPostsData }) => {
   return (
     <div className="m-4">
         <Profile />
-        <div class="flex flex-col items-center justify-center">
-          <div class="flex flex-col items-left justify-center lg:mx-64 md:mx-32 sm:mx-16">
+        <div className="flex flex-col items-center justify-center">
+          <div className="flex flex-col items-left justify-center lg:mx-64 md:mx-32 sm:mx-16">
             {allPostsData.map(({ id, date, title, description }) => (
-              <Article id={id} title={title} date={date} description={description}/>
+              <Article key={id} id={id} title={title} date={date} description={description}/>
             ))}
           </div>
         </div>
@@ -37,24 +39,8 @@ export default ({ allPostsData }) => {
   )
 }
 
-/**
-  return (
-      <section>
-        <h2>Blog</h2>
-        <ul>
-          {allPostsData.map(({ id, date, title }) => (
-            <li key={id}>
-              {title}
-              <br />
-              {id}
-              <br />
-              {date}
-            </li>
-          ))}
-        </ul>
-      </section>
-  )
- */
+Home.displayName = 'Home'
+export default Home
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData()
